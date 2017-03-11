0f7800291036e2029013aa738a7edd71bb691aa2
Troy Pracy <troyp7@gmail.com>
2017-03-11 23:23:13 +1100

    add module boilerplate and prefix

--- elisp-utils.el
+++ elisp-utils.el
@@ -1,5 +1,28 @@
+;;; elisp-utils.el ---
 
-(defun copy-md-sig-and-doc ()
+;; Copyright (C) 2017 Troy Pracy
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 2 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+
+(defun elu-copy-md-sig-and-doc ()
   "Extract signature and documentation for a function/macro in markdown format."
   (interactive)
   (let* ((def (sexp-at-point))
@@ -15,3 +38,8 @@
          )
     (kill-new md-def)
     (kill-new md-sig)))
+
+
+(provide 'elisp-utils)
+
+;;; elisp-utils ends here
